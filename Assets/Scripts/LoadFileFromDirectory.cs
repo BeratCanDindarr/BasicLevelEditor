98bf7a89e53537fdce0973a7f5c65ad87b4c9416
kabasakal <kabasakal@kabasakal.com>
2023-11-24 15:59:42 +0300

    Fixxed Null referans Problem

--- Assets/Scripts/LoadFileFromDirectory.cs
+++ Assets/Scripts/LoadFileFromDirectory.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 using UnityEditor;
 using UnityEditor.PackageManager.UI;
 using System.IO;
@@ -11,6 +11,7 @@ public class LoadFileFromDirectory : MonoBehaviour
     const string resourcesDir = "Assets/Resources/";
     const string levelDir = "Assets/Resources/Level";
     const string prefabDir = "Assets/Resources/Prefabs";
+    LevelData[] data;
 
     public void CreateScriptableObjectInDirectory(string fileName, LevelData levelData){
         if(CheckFileExists(levelDir)){
@@ -22,16 +23,16 @@ public class LoadFileFromDirectory : MonoBehaviour
         }
     }
 
-    public LevelData[] ResourcesLoadAll(string path){
+    // public LevelData[] ResourcesLoadAll(string path){
         
-        if(CheckFileExists(levelDir)){
-            LevelData[] levelData = Resources.LoadAll<LevelData>(path);
+    //     if(CheckFileExists(levelDir)){
+    //         LevelData[] levelData = Resources.LoadAll<LevelData>(path);
             
-            return levelData;
-        }
-        return null;
+    //         return levelData;
+    //     }
+    //     return null;
 
-    }
+    // }
 
     public GameObject LoadFromGameObject(string path){
         GameObject gameObject = null;
@@ -40,6 +41,27 @@ public class LoadFileFromDirectory : MonoBehaviour
         }
         return gameObject;
     }
+
+    public T[] ResourcesLoadAll<T>(string path){
+        
+        if(CheckFileExists(resourcesDir+path)){
+            data = Resources.LoadAll<LevelData>(path);
+        }
+        T[] result = (T[])Convert.ChangeType(data,typeof(T[]));
+        return result;
+        
+    }
+
+    public T[] test<T>(string testii){
+        Debug.Log(testii);
+        string[] deneme = new string[2];
+        deneme[0] = testii;
+        deneme[1] = "deneme";
+        T[] result = (T[])Convert.ChangeType( deneme, typeof(T[]));
+        Debug.Log(result);
+        return result;
+    }
+
 
     private bool CheckFileExists(string filePath){
         bool success = true;
